7e477ea8043352ce766f10bb921beacdb8562423
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-05-21 13:10:43 +0900

    Add workflows-from-workers

---
+++ packages/workflows-from-workers/workflows/src/index.ts
@@ -0,0 +1,45 @@
+/// <reference types="@cloudflare/workers-types" />
+
+import {
+  WorkerEntrypoint,
+  WorkflowEntrypoint,
+  type WorkflowEvent,
+  WorkflowStep,
+} from 'cloudflare:workers';
+
+interface Env {
+  WORKFLOW1: Workflow;
+}
+
+export class Workflow1 extends WorkflowEntrypoint<Env, Params> {
+  override async run(_event: WorkflowEvent<Params>, step: WorkflowStep) {
+    console.log('Run Workflow1');
+    await step.do('hello', async () => {
+      console.log('hello');
+      return Promise.resolve('world');
+    });
+  }
+}
+
+export class Workflow1Runner extends WorkerEntrypoint<Env> {
+  async createInstance() {
+    console.log('Create Workflow1');
+    const res = await this.env.WORKFLOW1.create();
+    return Promise.resolve(Response.json(res));
+  }
+}
+
+export default {
+  async fetch(req: Request, env: Env): Promise<Response> {
+    const url = new URL(req.url);
+
+    switch (url.pathname) {
+      case '/workflow1': {
+        const wf = await env.WORKFLOW1.create();
+        return Response.json(wf);
+      }
+    }
+
+    return Response.json({}, { status: 404 });
+  },
+};
