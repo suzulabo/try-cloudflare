65cdc2583d958a74fb8a772f7ca0d1782679b634
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-09-29 18:23:58 +0900

    Return hit status

--- packages/cache-data/src/index.ts
+++ packages/cache-data/src/index.ts
@@ -3,13 +3,16 @@ export default {
     const url = new URL(request.url);
 
     const cacheKey = `http://my-data/${url.pathname}`;
+    const age = url.searchParams.get('age') ?? '10';
+    const ng = url.searchParams.has('ng');
 
     const data = await (async () => {
       const res = await caches.default.match(cacheKey);
       if (res) {
         console.log(`CacheHit: [${request.url}]`);
         if (res.ok) {
-          return res.json();
+          const json: Record<string, string> = await res.json();
+          return { ...json, status: 'hit' };
         } else {
           return { status: 'ng' };
         }
@@ -17,19 +20,18 @@ export default {
 
       console.log(`CacheMiss: [${request.url}]`);
 
-      return { date: new Date().toISOString() };
+      return { date: new Date().toISOString(), status: 'miss', age, ng };
     })();
-
-    const age = url.searchParams.get('age') ?? '10';
-    const ng = url.searchParams.has('ng');
 
     const headers = { 'cache-control': `s-maxage=${age}` };
 
-    if (ng) {
-      console.log('Set NG Cache');
-      ctx.waitUntil(caches.default.put(cacheKey, new Response(null, { status: 404, headers })));
-    } else {
-      ctx.waitUntil(caches.default.put(cacheKey, Response.json(data, { headers })));
+    if (data.status !== 'hit') {
+      if (ng) {
+        console.log('Set NG Cache');
+        ctx.waitUntil(caches.default.put(cacheKey, new Response(null, { status: 404, headers })));
+      } else {
+        ctx.waitUntil(caches.default.put(cacheKey, Response.json(data, { headers })));
+      }
     }
 
     const res = new Response(JSON.stringify(data, undefined, 2));
